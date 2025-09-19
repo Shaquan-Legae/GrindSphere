25e1bcd24cc506574d3d1092d76664e0af688ed3
bandasihle <bandasihle710@gmail.com>
2025-09-19 12:09:55 +0200

    merge both dashboards into one branch

--- app/src/main/java/com/example/grindsphere/customer/CustomerDashboardActivity.kt
+++ app/src/main/java/com/example/grindsphere/customer/CustomerDashboardActivity.kt
@@ -5,13 +5,4 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.grindsphere.ui.theme.GrindSphereTheme
 
-class CustomerDashboardActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            GrindSphereTheme {
-                CustomerDashboard()
-            }
-        }
-    }
-}
+
